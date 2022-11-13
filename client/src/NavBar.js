afe70155f58c67248dc2fe222e9da3d65c2d5546
jzhen24 <j.zhen1024@gmail.com>
2022-11-12 23:46:20 -0500

    more buttons

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,29 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./NavBar.css";
-import axios from 'axios';
+import axios from "axios";
 
 function Navbar() {
-  const [item, setItem] = React.useState('');
-  const [description, setDescription] = React.useState('');
-  const [name, setName] = React.useState('');
-  const [email, setEmail] = React.useState('');
-  const [location, setLocation] = React.useState('');
+  const [item, setItem] = React.useState("");
+  const [description, setDescription] = React.useState("");
+  const [name, setName] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [location, setLocation] = React.useState("");
 
   const onSubmit = async (event) => {
-
     await axios.post(`http://localhost:8080/found/create`, {
       item,
       description,
       name,
       email,
-      location
+      location,
     });
 
-    setItem('');
-    setDescription('');
-    setName('');
-    setEmail('');
-    setLocation('');
+    setItem("");
+    setDescription("");
+    setName("");
+    setEmail("");
+    setLocation("");
   };
 
   return (
@@ -31,6 +30,9 @@ function Navbar() {
       <div className="middle">
         <a class="button" href="#popup1">
           <center>I FOUND</center>
+        </a>
+        <a class="button" href="#popup2">
+          <center>I LOST</center>
         </a>
       </div>
 
@@ -64,8 +66,76 @@ function Navbar() {
               <br></br>
               <p></p>
             </div>
-            <div class="popup-body1">
+            <div class="popup-body">
               <label for="fname">Location Found: </label>
+              <input
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body">
+              <label for="fname">Email: </label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body">
+              <label for="fname">Description: </label>
+              <input
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div className="submit-button">
+              <button className="btn btn-primary">Submit</button>
+            </div>
+            <a class="close" href="#">
+              &times;
+            </a>
+          </form>
+        </div>
+      </div>
+      <div id="popup2" class="overlay">
+        <div class="popup">
+          <h2>
+            <center>LOST</center>
+          </h2>
+          <a class="close" href="#">
+            &times;
+          </a>
+          <form onSubmit={onSubmit}>
+            <div class="content">
+              <label for="fname">Item Name: </label>
+              <input
+                value={item}
+                onChange={(e) => setItem(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body">
+              <label for="fname">Personal Name: </label>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body1">
+              <label for="fname">Location Lost: </label>
               <input
                 value={location}
                 onChange={(e) => setLocation(e.target.value)}
