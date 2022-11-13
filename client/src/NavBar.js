289bdfabc4d4b3ac30881e8db6f772ee71d2188e
Steven Pham <steventuanpham@gmail.com>
2022-11-12 20:26:03 -0500

    Adding functionality to I Found navbar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,7 +1,31 @@
 import React, { useState, useEffect } from "react";
 import "./NavBar.css";
+import axios from 'axios';
 
 function Navbar() {
+  const [item, setItem] = React.useState('');
+  const [description, setDescription] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [location, setLocation] = React.useState('');
+
+  const onSubmit = async (event) => {
+
+    await axios.post(`http://localhost:8080/found/create`, {
+      item,
+      description,
+      name,
+      email,
+      location
+    });
+
+    setItem('');
+    setDescription('');
+    setName('');
+    setEmail('');
+    setLocation('');
+  };
+
   return (
     <div className="Navbar">
       <div className="middle">
@@ -19,35 +43,64 @@ function Navbar() {
           <a class="close" href="#">
             &times;
           </a>
-          <div class="content">
-            <label for="fname">Item Name: </label>
-            <input type="text" id="fname" name="mylable"></input>
-            <br></br>
-            <p></p>
-          </div>
-          <div class="popup-body">
-            <label for="fname">Personal Name: </label>
-            <input type="text" id="fname" name="fname"></input>
-            <br></br>
-            <p></p>
-          </div>
-          <div class="popup-body1">
-            <label for="fname">Location Found: </label>
-            <input type="text" id="fname" name="fname"></input>
-            <br></br>
-            <p></p>
-          </div>
-          <div class="popup-body2">
-            <label for="fname">Email: </label>
-            <input type="text" id="fname" name="fname"></input>
-            <br></br>
-          </div>
-          <div className="submit-button">
-            <input type="submit" value="Submit" />
-          </div>
-          <a class="close" href="#">
-            &times;
-          </a>
+          <form onSubmit={onSubmit}>
+            <div class="content">
+              <label for="fname">Item Name: </label>
+              <input
+                value={item}
+                onChange={(e) => setItem(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body">
+              <label for="fname">Personal Name: </label>
+              <input
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body1">
+              <label for="fname">Location Found: </label>
+              <input
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body2">
+              <label for="fname">Email: </label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div class="popup-body3">
+              <label for="fname">Description: </label>
+              <input
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="form-control"
+              />
+              <br></br>
+              <p></p>
+            </div>
+            <div className="submit-button">
+              <button className="btn btn-primary">Submit</button>
+            </div>
+            <a class="close" href="#">
+              &times;
+            </a>
+          </form>
         </div>
       </div>
     </div>
